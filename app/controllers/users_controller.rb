733022d2a60206d9c35b5548296699b9290f7807
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-07-20 23:23:22 +0000

    modified login..erb and users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,21 @@
 class UsersController < ApplicationController
 
-  # what routes do I need for login?
-  # route: render to them the login page
+# what routes do I need for login?
+# route: render the login page
   get '/login' do
     erb :login
   end
-  # what routes do I need for signup?
-  get 'signup' do
 
+# creating a session (key, value pair to the session hash)
+# receive the login form, find the user, log user in
+  post '/login' do
+    binding.pry
   end
 
 
+# what routes do I need for signup?
+  get 'signup' do
+
+  end
 
 end
