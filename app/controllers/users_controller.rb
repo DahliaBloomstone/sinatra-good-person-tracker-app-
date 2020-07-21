3de94e5a76a530c4943ec43cc2bd14b4024ef5a9
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-07-21 00:24:48 +0000

    get and post login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,13 +10,24 @@ class UsersController < ApplicationController
 # receive the login form, find the user, log user in
 # Steps: Find user, authenticate user, log in user, redirect to user landing page
   post '/login' do
-    @user = User.find_by(email: params[:email])
-    @user.authenticate(params[:password])
+  @user = User.find_by(email: params[:email])
+  if @user.authenticate(params[:password])
+    session[:user_id] = @user.id #actually logging the user in
+    puts session
+    redirect "users/#{@user.id}"
+  else
+
+    end
   end
 
 # what routes do I need for signup?
   get 'signup' do
 
+  end
+
+# show route
+  get '/users/:id' do
+    "user show route"
   end
 
 end
