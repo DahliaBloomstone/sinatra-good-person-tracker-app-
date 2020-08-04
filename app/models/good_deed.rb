105c87e34bee349a2e3992607166f03dee5bc2ba
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-08-04 03:12:24 -0400

    Update good_deed.rb

--- app/models/good_deed.rb
+++ app/models/good_deed.rb
@@ -1,9 +1,13 @@
 class GoodDeed < ActiveRecord::Base
 
+#ActiveRecord Association 
   belongs_to :user
 
+#human readable time
 def formatted_created_at
   self.created_at.strftime("%A, %d %b %Y %l:%M %p")
   end
 
 end
+
+#user is able to create, read, update, and destroy the resource (good deed) that belongs_to the user
