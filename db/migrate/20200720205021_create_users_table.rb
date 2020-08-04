edb605d70b4833ed0c4a5368d393887ab5883990
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-08-04 03:15:07 -0400

    Update 20200720205021_create_users_table.rb

--- db/migrate/20200720205021_create_users_table.rb
+++ db/migrate/20200720205021_create_users_table.rb
@@ -8,3 +8,10 @@ class CreateUsersTable < ActiveRecord::Migration
     end
   end
 end
+
+
+
+# Why do we use the bcrypt gem and what is the significance of adding the password_digest column to the User model in the database?
+# To avoid passwords from being stolen, the password is encrypted before stored in your database. This is done by bcrypt Ruby gem.
+# The bcypt Ruby gem provides you with has_secure_password method. The has_secure_password method encrypts passwords by hashing and salting the passwords and generate ‘password_digest’.
+# The has_secure_password method in turn gives you, #authenticate method, which you can use to authenticate passwords.
