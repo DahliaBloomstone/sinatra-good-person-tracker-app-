69f348f565fdbbc316146e0b70d1b6d17013091d
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-08-06 06:48:50 -0400

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,23 +14,25 @@ class ApplicationController < Sinatra::Base
   get "/" do
   #  binding.pry
     if logged_in?
-      redirect "/users/#{current_user.id}" #if user is logged in, their session will be saved in the app by their user id
+      redirect "/users/#{current_user.id}" # if user is logged in, their session will be saved in the app by their user id
     else
-    erb :welcome #else, they are shown the welcome page 
+    erb :welcome # else, they are shown the welcome page 
   end
 end
 
   helpers do
 
- def logged_in? #boolean reflection of an objects truthyness
+ def logged_in? # boolean reflection of an objects truthyness
    !!current_user
   end
 
-  def current_user
-  @current_user ||= User.find_by(id: session[:user_id])
+  def current_user # return current user if there is one 
+  @current_user ||= User.find_by(id: session[:user_id]) # find current user from the session hash. key value pair (user id) 
+          # find_by queries the database. returns nil if there is not a key value pair that matches 
+        # @current_user ||= (or equals) first time current user is referenced, instance variable will be created and assigned if the user is found by their id 
   end
 
-def authorized_to_edit?(good_deed)
+def authorized_to_edit?(good_deed) #if the current user is authorized to edit, they can edit their own good deeds but not other users
   good_deed.user == current_user
     end
 
